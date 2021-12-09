389097df7d48cafd39d1f293a94885af2747d7d3
Jose <jarcodallo@gmail.com>
2021-12-09 14:06:04 -0300

    Near provider base methods (#2)
    Near provider base methods

--- src/near-rpc-provider.ts
+++ src/near-rpc-provider.ts
@@ -1,3 +1,163 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
+import { Logger } from '@ethersproject/logger'
+import { deepCopy, getStatic } from '@ethersproject/properties'
+import { JsonRpcProvider, Network, Networkish } from '@ethersproject/providers'
+import { fetchJson } from '@ethersproject/web'
+import { logger } from './logger'
+import { getNetwork } from './networks'
 
-export declare class NearRpcProvider extends JsonRpcProvider {}
+export interface RpcResponse {
+  jsonrpc: string
+  id: string
+  result?: Record<string, any>
+  error?: {
+    name: string
+    code: number
+    message: string
+    data: string
+    cause: {
+      name: string
+      info: Record<string, any>
+    }
+  }
+}
+
+export class RpcError extends Error {
+  public readonly type: string
+  public readonly code: number
+  public readonly data: string
+
+  constructor(message: string, type: string, code: number, data: string) {
+    super(message)
+    this.name = this.constructor.name
+    this.type = type
+    this.code = code
+    this.data = data
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+function getResult(payload: RpcResponse): any {
+  if (payload.error) {
+    throw new RpcError(payload.error.message, payload.error.name, payload.error.code, payload.error.data)
+  }
+
+  return payload.result
+}
+
+export class NearRpcProvider extends JsonRpcProvider {
+  constructor(network?: Networkish) {
+    const baseUrl = getStatic<(network?: Network | null) => string>(new.target, 'getBaseUrl')(getNetwork(network))
+
+    super(baseUrl, network)
+
+    this._nextId = 52
+  }
+
+  static defaultUrl(): string {
+    return 'http://localhost:3030'
+  }
+
+  static getBaseUrl(network?: Network | null): string {
+    switch (network ? network.name : 'invalid') {
+      case 'near-mainnet':
+        return 'https://rpc.mainnet.near.org'
+      case 'near-mainnet':
+        return 'https://rpc.testnet.near.org'
+      case 'near-betanet':
+        return 'https://rpc.betanet.near.org'
+    }
+
+    return logger.throwArgumentError('unsupported network', 'network', network)
+  }
+
+  getNextId(): number {
+    this._nextId += 1
+
+    return this._nextId
+  }
+
+  async _uncachedDetectNetwork(): Promise<Network> {
+    let chainId = null
+
+    try {
+      chainId = await this.send('status', [])
+    } catch (error) {
+      try {
+        chainId = await this.send('EXPERIMENTAL_genesis_config', [])
+      } catch (error) {}
+    }
+
+    if (chainId !== null) {
+      try {
+        const network = getNetwork(parseInt(Buffer.from(chainId).toString('hex'), 16))
+
+        if (!network) {
+          throw new Error(`Invalid network chainId ${chainId}`)
+        }
+
+        return network
+      } catch (error) {
+        return logger.throwError('could not detect network', Logger.errors.NETWORK_ERROR, {
+          chainId: chainId,
+          event: 'invalidNetwork',
+          serverError: error,
+        })
+      }
+    }
+
+    return logger.throwError('could not detect network', Logger.errors.NETWORK_ERROR, {
+      event: 'noNetwork',
+    })
+  }
+
+  async send(method: string, params: Array<any>): Promise<any> {
+    const request = {
+      method: method,
+      params: params,
+      id: this.getNextId(),
+      jsonrpc: '2.0',
+    }
+
+    this.emit('debug', {
+      action: 'request',
+      request: deepCopy(request),
+      provider: this,
+    })
+
+    const cache = ['status', 'EXPERIMENTAL_genesis_config', 'block'].indexOf(method) >= 0
+    if (cache && this._cache[method]) {
+      return this._cache[method]
+    }
+
+    try {
+      const result = await fetchJson(this.connection, JSON.stringify(request), getResult)
+
+      this.emit('debug', {
+        action: 'response',
+        request: request,
+        response: result,
+        provider: this,
+      })
+
+      // Cache the fetch, but clear it on the next event loop
+      if (cache) {
+        this._cache[method] = result
+        setTimeout(() => {
+          this._eventLoopCache = {}
+        }, 0)
+      }
+
+      return result
+    } catch (err) {
+      this.emit('debug', {
+        action: 'response',
+        error: err,
+        request: request,
+        provider: this,
+      })
+
+      throw err
+    }
+  }
+}
